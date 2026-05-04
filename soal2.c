19e62a0d1e2d3e6df4f8cd8872e7236f94ed1049
Starlightt-ken <kennypramanik2@gmail.com>
2026-05-04 10:06:22 +0700

    Update soal2.c -jawaban2

--- soal2.c
+++ soal2.c
@@ -16,53 +16,65 @@ typedef struct gerbong{
     struct gerbong *next; 
 }gerbong;
 
+//fungsi untuk membuat node baru 
+struct gerbong *newnode(int data){
+    struct gerbong *node = (struct gerbong*) malloc (sizeof(struct gerbong));
+    node->data = data; 
+    node-> next = NULL;
+    return node;
+}
 //membuat fungsi untuk menambahkan elemen 
 void insert(struct gerbong **head, int data){
-    struct gerbong *list = (struct gerbong*)malloc(sizeof(gerbong)); //membuat banyak list pertama
+    struct gerbong *newnode = (struct gerbong*)malloc(sizeof(struct gerbong)); //membuat banyak list pertama
 
-    list->data = data; 
-    if (*head == NULL){ //kalau misalkan listnya kosong, penambahan akan membuat elemen pertama menjadi head
-        *head = list; 
-    } else {
-        struct gerbong *temp = *head; 
-        while (temp->next != NULL){
-            temp = temp->next;
-        }
-        temp->next = list;
+    struct gerbong *last = *head;
+
+    newnode->data = data;
+    newnode->next = NULL;
+
+    if(*head == NULL){
+        *head = newnode;
+        return;
     }
+    while(last->next != NULL)
+        last = last->next;
+    last->next = newnode;
+    return;
 }
 
 //membuat fungsi merged
 struct gerbong *sort_merge(struct gerbong *head1, struct gerbong *head2){
-    //jika semua list kosong 
-    if (head1 == NULL && head2 == NULL){
-        printf("MERGED EMPTY");
-        return 0;
-    }
-    //cek jika salah satu list kosong dan list lainnya hanya berisi 1 elemen
-    if (head1 == NULL){
-        return head2; 
-    }
-    if (head2 == NULL){
-        return head1; 
+    if (!head1) return head2;
+    if (!head2) return head1;
+
+    // Create a dummy node to store the result
+    struct gerbong dummy;
+    struct gerbong* tail = &dummy;
+
+    // Traverse both linked lists, adding smaller elements to the result list
+    while (head1 && head2) {
+        if (head1->data < head2->data) {
+            tail->next = head1;
+            head1 = head1->next;
+        } else {
+            tail->next = head2;
+            head2 = head2->next;
+        }
+        tail = tail->next;
     }
 
-    //proses merging berdasarkan nilai elemen yang lebih kecil 
-    if (head1->data <= head2->data){
-        head1->next = sort_merge(head1, head2->next);
-        return head2;
-    }
+    // If either of the linked lists is not fully processed, append the rest of the elements
+    tail->next = head1 ? head1 : head2;
+
+    // Return the result
+    return dummy.next;
 }
 
-void cetak(struct gerbong *list){
-    while(list != NULL){
-        printf("%d", list->data);
-        if (list->next != NULL){
-            printf("->");
-        }
-        list = list->next; 
+void cetak(struct gerbong *head){
+    while(head){
+        printf("%d ", head->data);
+        head = head ->next;
     }
-    printf("\n");
 }
 
 int main(){
@@ -77,20 +89,19 @@ int main(){
     //inisialisasi data
     int data1;
     int data2;
-    int count1 = 0; 
-    int count2 = 0;
 
     //banyak input datanya
     printf("input banyak data1");
     scanf("%d", &n);
 
+    int count1 = 0; 
+    int count2 = 0;
+
     //membuat linked list yang ukurannya sesuai dengan n dan juga m
-    
     //linked list 1 
-    while(count1 < n) {
+    for (int i = 0; i < n; i++) {
         scanf("%d", &data1);
         insert(&head1, data1);
-        count1++;
     }
 
     //banyak input datanya
@@ -98,10 +109,9 @@ int main(){
     scanf("%d", &m);
 
     //linked list 2
-    while(count2 < m){
+    for(int i = 0; i < m; i++){
         scanf("%d", &data2);
         insert(&head2, data2);
-        count2++;
     }
     struct gerbong *list3 = sort_merge(head1, head2);
     cetak(list3);
